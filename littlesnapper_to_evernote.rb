5e357a7dd6555772ccaf8a0a5ec6f65cb80e73ae
Ian Collins <ian@powerset.com>
2010-01-07 14:03:29 -0800

    basic reading snap files from snapper library

--- littlesnapper_to_evernote.rb
+++ littlesnapper_to_evernote.rb
@@ -1,1 +1,16 @@
-print ARGV[0]
+require 'rexml/document'
+
+class Evernote
+  def new_note_from_file(file)
+    puts "gonna create a note from file: " + file
+  end
+end
+
+LS_LIBRARY = ARGV[0]
+
+Dir.chdir(LS_LIBRARY)
+
+Dir.entries(LS_LIBRARY)[2..-1].each do |entry|
+  puts Dir.entries(entry) if File::directory?(entry)
+  puts "---"
+end
