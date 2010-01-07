4fdc6e20528f93b6b7e93c034a111b57c00a9a88
Ian Collins <ian@powerset.com>
2010-01-07 15:47:11 -0800

    pulling data from the xmls

--- littlesnapper_to_evernote.rb
+++ littlesnapper_to_evernote.rb
@@ -1,4 +1,5 @@
-require 'rexml/document'
+require 'rubygems'
+require 'libxml'
 
 class Evernote
   def new_note_from_file(file)
@@ -8,9 +9,16 @@ end
 
 LS_LIBRARY = ARGV[0]
 
-Dir.chdir(LS_LIBRARY)
-
 Dir.entries(LS_LIBRARY)[2..-1].each do |entry|
-  puts Dir.entries(entry) if File::directory?(entry)
-  puts "---"
+  Dir.chdir(LS_LIBRARY)
+  if File::directory?(entry)
+    snap_files  = Dir.entries(entry)
+    other_files = snap_files.find_all {|f| !['info.plist','.','..'].include?(f)}
+
+    Dir.chdir(entry)
+    doc = XML::Document.file(Dir.pwd + '/info.plist')
+    tags   = doc.find('/plist/dict/key[. = "tags"]/following-sibling::array/string').map{|x| x.content}.to_a
+    rating = doc.find('/plist/dict/key[. = "rating"]/following-sibling::integer').first.content rescue 0
+    date   = doc.find('/plist/dict/key[. = "date"]/following-sibling::date').first.content rescue Date.today.to_s
+  end
 end
